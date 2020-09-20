7d57d0e40cc734e45290ce4fb72bbd115ee97e30
Ankur Rastogi <ank.rastogi15@gmail.com>
2020-09-20 16:20:31 -0700

    clean up code, remove console statements, use prettier formatting

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-const TURN_DURATION = 3;
-const PAUSE_TEXT = 'Pause';
-const PLAY_TEXT = 'Resume play';
+const TURN_DURATION = 60;
+const PAUSE_TEXT = "Pause";
+const PLAY_TEXT = "Resume play";
 
-let secondsLeft = TURN_DURATION + 0.1;
+// needed to account for button press time
+const START_BUFFER = 0.1;
+let secondsLeft = TURN_DURATION + START_BUFFER;
 let paused = false;
 let countdown;
-
-
 
 // TODO: add decimal point when it's 10 seconds or less
 
 function resumeTimer() {
   countdown = setInterval(() => {
     // don't use equality operator for double
-    if (0.100 < secondsLeft  < 0.200) {
-      document.getElementById("seconds").textContent = "Time's up!"
+    if (0.1 < secondsLeft < 0.2) {
+      document.getElementById("seconds").textContent = "Time's up!";
       return;
     }
-    console.log(secondsLeft);
+
     secondsLeft = secondsLeft - 0.1;
     const decimalPlaces = secondsLeft < 10 ? 1 : 0;
     document.getElementById("seconds").textContent = secondsLeft.toFixed(decimalPlaces);
   }, 100);
-
-
-
 }
 
 function togglePause() {
   if (paused) {
     paused = false;
-    document.getElementById("status-indicator").textContent = '';
+    document.getElementById("status-indicator").textContent = "";
     document.getElementById("pause-play").textContent = PAUSE_TEXT;
     resumeTimer();
   } else {
     paused = true;
-    document.getElementById("status-indicator").textContent = '||';
+    document.getElementById("status-indicator").textContent = "||";
     document.getElementById("pause-play").textContent = PLAY_TEXT;
     clearInterval(countdown);
   }
@@ -43,7 +40,7 @@ function togglePause() {
 
 function start() {
   paused = false;
-  document.getElementById("status-indicator").textContent = '';
+  document.getElementById("status-indicator").textContent = "";
   if (secondsLeft < TURN_DURATION) {
     clearInterval(countdown);
     secondsLeft = TURN_DURATION;
