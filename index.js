21a3db9a4d2e38bf2e065047309128e86ad0a448
Ankur Rastogi <ank.rastogi15@gmail.com>
2020-09-20 16:14:31 -0700

    add sub-10s decimal timing, end of turn message, and js constants

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-let secondsLeft = 60;
+const TURN_DURATION = 3;
+const PAUSE_TEXT = 'Pause';
+const PLAY_TEXT = 'Resume play';
+
+let secondsLeft = TURN_DURATION + 0.1;
 let paused = false;
 let countdown;
 
+
+
+// TODO: add decimal point when it's 10 seconds or less
+
+function resumeTimer() {
+  countdown = setInterval(() => {
+    // don't use equality operator for double
+    if (0.100 < secondsLeft  < 0.200) {
+      document.getElementById("seconds").textContent = "Time's up!"
+      return;
+    }
+    console.log(secondsLeft);
+    secondsLeft = secondsLeft - 0.1;
+    const decimalPlaces = secondsLeft < 10 ? 1 : 0;
+    document.getElementById("seconds").textContent = secondsLeft.toFixed(decimalPlaces);
+  }, 100);
+
+
+
+}
+
 function togglePause() {
   if (paused) {
-    document.getElementById("status-indicator").textContent = '';
     paused = false;
-    countdown = setInterval(() => {
-      secondsLeft--;
-      document.getElementById("seconds").textContent = secondsLeft;
-    }, 1000);
+    document.getElementById("status-indicator").textContent = '';
+    document.getElementById("pause-play").textContent = PAUSE_TEXT;
+    resumeTimer();
   } else {
     paused = true;
     document.getElementById("status-indicator").textContent = '||';
+    document.getElementById("pause-play").textContent = PLAY_TEXT;
     clearInterval(countdown);
   }
 }
@@ -20,12 +44,10 @@ function togglePause() {
 function start() {
   paused = false;
   document.getElementById("status-indicator").textContent = '';
-  if (secondsLeft < 60) {
+  if (secondsLeft < TURN_DURATION) {
     clearInterval(countdown);
-    secondsLeft = 61;
+    secondsLeft = TURN_DURATION;
+    document.getElementById("pause-play").textContent = PAUSE_TEXT;
   }
-  countdown = setInterval(() => {
-    secondsLeft--;
-    document.getElementById("seconds").textContent = secondsLeft;
-  }, 1000);
+  resumeTimer();
 }
