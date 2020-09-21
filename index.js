0c624832e8779602dd0e366d4be2eeaa4100db06
Ben Stanfield <bnstnfld@gmail.com>
2020-09-20 17:30:02 -0700

    mirrored board

--- index.js
+++ index.js
@@ -8,41 +8,61 @@ let secondsLeft = TURN_DURATION + START_BUFFER;
 let paused = false;
 let countdown;
 
+// converts htmlCollection -> Array
+function getElementsAsArray(className) {
+  let elements = document.getElementsByClassName(className);
+  return Array.from(elements);
+}
+
 function resumeTimer() {
   countdown = setInterval(() => {
     // don't use equality operator for double
     if (0.1 < secondsLeft < 0.2) {
-      document.getElementById("seconds").textContent = "Time's up!";
+      const counters = getElementsAsArray('seconds');
+      counters.map(counter => counter.textContent = "Time's up!");
       return;
     }
 
     secondsLeft = secondsLeft - 0.1;
     const decimalPlaces = secondsLeft < 10 ? 1 : 0;
-    document.getElementById("seconds").textContent = secondsLeft.toFixed(decimalPlaces);
+    const counters = getElementsAsArray('seconds');
+    counters.map(counter => counter.textContent = secondsLeft.toFixed(decimalPlaces));
   }, 100);
 }
 
 function togglePause() {
   if (paused) {
     paused = false;
-    document.getElementById("status-indicator").style.display = 'none';
-    document.getElementById("pause-play").textContent = PAUSE_TEXT;
+    // toggle pause emoji
+    const indicators = getElementsAsArray('status-indicator');
+    indicators.map(i => i.style.display = 'none');
+
+    // toggle pause/play text on btn
+    const buttons = getElementsAsArray('pause-play');
+    buttons.map(b => b.textContent = PAUSE_TEXT);
     resumeTimer();
   } else {
     paused = true;
-    document.getElementById("status-indicator").style.display = 'block';
-    document.getElementById("pause-play").textContent = PLAY_TEXT;
+    // toggle pause emoji
+    const indicators = getElementsAsArray('status-indicator');
+    indicators.map(i => i.style.display = 'block');
+
+    // toggle pause/play text on btn
+    const buttons = getElementsAsArray('pause-play');
+    buttons.map(b => b.textContent = PLAY_TEXT);
     clearInterval(countdown);
   }
 }
 
 function start() {
   paused = false;
-  document.getElementById("status-indicator").style.display = 'none';
+  const indicators = getElementsAsArray('status-indicator');
+  indicators.map(i => i.style.display = 'none');
   if (secondsLeft < TURN_DURATION) {
     clearInterval(countdown);
     secondsLeft = TURN_DURATION;
-    document.getElementById("pause-play").textContent = PAUSE_TEXT;
+    const buttons = getElementsAsArray('pause-play');
+    buttons.map(b => b.textContent = PAUSE_TEXT);
   }
   resumeTimer();
 }
